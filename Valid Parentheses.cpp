6231fb7e64672931f2cb2e0c452f151f267cf6fa
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2025-03-15 00:34:18 +0530

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+bool isValidParenthesis(string s)
+{
+    // Write your code here.
+
+    stack<char>st;
+
+    for(auto it:s){
+
+        if(it=='(' || it =='{' || it =='['){
+            st.push(it);
+        }
+
+        else{
+            if(st.size()==0){
+                return false;
+            }
+            char ch = st.top();
+            st.pop();
+
+            if((it==')' && ch=='(') || (it=='}' && ch=='{') || (it==']' && ch=='[') ){
+                continue;
+            }
+            else{
+                return false;
+            }
+
+        }
+        
+        
+    }
+    return true;
+
+
+}
